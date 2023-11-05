5030f17fef00433e03553b362f57c0461baa570e
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-11-05 10:45:25 +0200

    added filtration by make

--- test-task/src/components/CatalogList/CatalogList.jsx
+++ test-task/src/components/CatalogList/CatalogList.jsx
@@ -8,18 +8,29 @@ import {
   StyledCatalogList,
 } from "./CatalogList.styled";
 import { selectAllCars } from "../../redux/Cars/selectors";
+import { useState } from "react";
 
 
 export const CatalogList = () => {
   const cars = useSelector(selectAllCars);
+  const [selectedMake, setSelectedMake] = useState("all");
+
+  const makes = cars.map(car => car.make);
+  const uniqueMakes = [...new Set(makes)];
+
+  const filteredCars = selectedMake === "all" ? cars : cars.filter(car => car.make === selectedMake);
+
+  const handleSelectMake = (make) => {
+    setSelectedMake(make);
+  };
 
 
   return (
     <StyledCarsList>
       <StyledCatalogContainer>
-        <SearchParamsForm />
+        <SearchParamsForm makes={uniqueMakes} onSelectMake={handleSelectMake}/>
         <StyledCatalogList>
-          {cars && cars.map((car) => <CatalogCart key={car.id} data={car} />)}
+          {filteredCars && filteredCars.map((car) => <CatalogCart key={car.id} data={car} />)}
         </StyledCatalogList>
         <PaginationBtn type="button">Load more</PaginationBtn>
       </StyledCatalogContainer>
--- test-task/src/components/SearchForm/SearchParamsForm.jsx
+++ test-task/src/components/SearchForm/SearchParamsForm.jsx
@@ -1,19 +1,35 @@
-import { StyledCarSelect, StyledCarSelectTitle, StyledCarSelectWrap, StyledSearchBtn, StyledSearchForm, StyledSearchFormInput, StyledSearchFormInputWrap } from './SearchParamsForm.styled';
+import {
+  StyledCarSelect,
+  StyledCarSelectTitle,
+  StyledCarSelectWrap,
+  StyledSearchBtn,
+  StyledSearchForm,
+  // StyledSearchFormInput,
+  // StyledSearchFormInputWrap,
+} from "./SearchParamsForm.styled";
 
+export const SearchParamsForm = (
+  { makes, onSelectMake }
+) => {
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    onSelectMake(document.getElementById('carName').value);
+  };
 
-export const SearchParamsForm = () => {
   return (
-    <StyledSearchForm>
+    <StyledSearchForm onSubmit={handleFormSubmit}>
       <StyledCarSelectWrap>
         <StyledCarSelectTitle>Car brand</StyledCarSelectTitle>
-        <StyledCarSelect className="carName" name="carName" id="carName">
-          <option value="javascript">All</option>
-          <option value="python">Python</option>
-          <option value="c++">C++</option>
-          <option value="java">Java</option>
+        <StyledCarSelect
+          className="carName"
+          name="carName"
+          id="carName"
+        >
+          <option value="all">All</option>
+          {makes && makes.map((make) => <option value={make}>{make}</option>)}
         </StyledCarSelect>
       </StyledCarSelectWrap>
-      <StyledCarSelectWrap>
+      {/* <StyledCarSelectWrap>
         <StyledCarSelectTitle>Price/ 1 hour</StyledCarSelectTitle>
         <StyledCarSelect className="carPrice" name="carPrice" id="carPrice">
           <option value="10">10</option>
@@ -42,8 +58,8 @@ export const SearchParamsForm = () => {
             placeholder="To"
           />
         </StyledSearchFormInputWrap>
-      </StyledCarSelectWrap>
-      <StyledSearchBtn type='submit'>Search</StyledSearchBtn>
+      </StyledCarSelectWrap> */}
+      <StyledSearchBtn type="submit">Search</StyledSearchBtn>
     </StyledSearchForm>
   );
 };
