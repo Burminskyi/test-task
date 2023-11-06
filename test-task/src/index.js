2eedf556090c5fa1fedb40349ede84ffbfee5519
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-11-06 18:29:50 +0200

    Added basename

--- test-task/src/index.js
+++ test-task/src/index.js
@@ -11,7 +11,7 @@ import { persistor, store } from "./redux/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/test-task">
         <PersistGate persistor={persistor}>
           <App />
         </PersistGate>
